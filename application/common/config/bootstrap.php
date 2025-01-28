4a6e60cd3389b839aad17cd4a36c2fa04c43649f
alx <world.zay@gmail.com>
2025-01-28 17:57:14 +0300

    Updated config bootstrap, deleted config mode.php

--- application/common/config/bootstrap.php
+++ application/common/config/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set(env('APP_TZ'));
+
 define('PRODUCTION', 10);
 define('STAGE', 20);
 define('TESTING', 30);
@@ -20,3 +22,17 @@ define('IS_WATERMARK', true);
 define('IS_IMG_CACHE', false);
 
 define('STRICT_MODE', false);
+
+if (PHP_SAPI === 'cli') {
+    define('MODE', 'cli');
+} elseif (strpos($_SERVER['REQUEST_URI'], '/api/') === 0) {
+    define('MODE', 'api');
+} else {
+    define('MODE', 'web');
+}
+
+define('ROUTE_PATHS', [
+    CONFIG . 'simple_routes/' . MODE . '.php',
+    APPPATH . 'auth/config/simple_routes.php',
+    CONFIG . 'simple_routes/common.php',
+]); 

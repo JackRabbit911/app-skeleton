4ef6ad2f6c1a4af82107341813ef9d5aa94190a3
alx <world.zay@gmail.com>
2025-02-13 14:36:09 +0300

    Updated config sidebar.php

--- application/docs/Guide/data/sidebar.php
+++ application/docs/Guide/data/sidebar.php
@@ -2,11 +2,95 @@
 
 return [
     'Getting started' => [
-        'installation',
-        'settings' => [
-            'config',
+        'Installation',
+        'Settings' => [
             'enviroment',
+            'config',
         ],
     ],
-    'Application life cycle'
+    'Structure' => [
+        'Namespaces',
+        'index.php & console',
+    ],
+    'Application life cycle',
+    'Routing' => [
+        'config/routes.php',
+        'Attributes',
+        'Handler (Controller)',
+        'Container',
+    ],
+    'Pipeline' => [
+        'config/pipeline.php',
+        'Middleware',
+    ],
+    'Sessions' => [
+        'config/session.php',
+        'Session',
+        'SessionMiddleware',
+        'Driver',
+    ],
+    'Controller' => [
+        'BaseController',
+        'WebController',
+        'ApiController',
+    ],
+    'Template' => [
+        'config/template.php',
+        'Usage',
+        'Template\App',
+        'Component',
+        'ComponentForm',
+    ],
+    'Validation' => [
+        'Usage',
+        'How it works',
+        'Response',
+        'ValidationHandler',
+        'Custom functions',
+        'Messages',
+    ],
+    'Helpers' => [
+        'Helper namespace',
+        'library.php',
+    ],
+    'Model & Entity' => [
+        'Model',
+        'Entity',
+        'Saveble',
+    ],
+    'Mailer' => [
+        'Mail transfer',
+        'Mailer',
+        'Email entity',
+        'Settings',
+        'Usage',
+    ],
+    'PostProcess' => [
+        'Usage',
+        'Attributes',
+        'EventProvider',
+        'Listener & Observer',
+    ],
+    'Queues, Tasks, Jobs' => [
+        'Usage',
+        'Model',
+        'Cron ticker',
+        'Logging',
+    ],
+    'Console' => [
+        'Commands',
+        'Custom commands',
+    ],
+    'I18n' => [
+        'config/i18n.php',
+        'How it works',
+        'gettext',
+    ],
+    'Modules' => [
+        'Auth',
+        'Rating',
+        'Comments',
+        'Admin panel',
+    ],
+    'Deploy'
 ];
